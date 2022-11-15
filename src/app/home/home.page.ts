bb6e193c7eb2a6c2c9867a2a3f14f5174e456610
ICT DOST XI <pantojanpaul7916@gmail.com>
2022-11-15 15:43:10 +0800

    2nd Day Dost mobile Dev

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  employees = [
+    {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "color":"primary",
+    "pic": "assets/img/2.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "color":"secondary",
+    "pic": "assets/img/3.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "color":"tertiary",
+    "pic": "assets/img/4.jpg"
+  }];
+
+
   constructor() {}
 
 }
